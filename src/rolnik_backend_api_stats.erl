ee49a4bcf09687f9e67a20346e6e29d965487462
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2019-03-26 12:42:29 +0100

    Stub /stats data with jsone

--- src/rolnik_backend_api_stats.erl
+++ src/rolnik_backend_api_stats.erl
@@ -1,16 +1,20 @@
 -module(rolnik_backend_api_stats).
 
-%% cowboy callbacks
+-compile([export_all]).
+
+%% Cowboy callbacks
 -export([init/2]).
 
 %%
-%% cowboy callbacks
+%% Cowboy callbacks
 %%
+
 init(Req0, State) ->
     Headers = #{<<"content-type">> => <<"text/json">>,
                 <<"access-control-allow-origin">> => <<"*">>},
     Req = cowboy_req:reply(200, Headers,
-                           hardcoded_development_stub(),
+                           %hardcoded_development_stub(),
+                           jsone_stub(),
                            Req0),
     {ok, Req, State}.
 
@@ -18,19 +22,37 @@ init(Req0, State) ->
 %% Internal
 %%
 
-hardcoded_development_stub() ->
-    <<"{"
-      "  \"temp\": [", (device_temp())/bytes, "],"
-      "  \"humidity\": [", (device_humidity())/bytes, "]"
-      "}">>.
+%hardcoded_development_stub() ->
+%    <<"{"
+%      "  \"temp\": [", (device_temp())/bytes, "],"
+%      "  \"humidity\": [", (device_humidity())/bytes, "]"
+%      "}">>.
 
-device_temp() -> <<"{ \"devname\": \"device1\","
-                   "  \"data\": [ {\"tstamp\": \"2014-04-07T13:58:10.104Z\","
-                   "           \"value\": 36.6}] }">>.
+jsone_stub() ->
+    jsone:encode(#{temp => [device_temp(<<"device1">>)],
+                   humidity => [device_humidity(<<"device1">>)]}).
 
-device_humidity() -> <<"{ \"devname\": \"device1\","
-                       "  \"data\": [ {\"tstamp\": \"2014-04-07T13:58:10.104Z\","
-                       "           \"value\": 40}] }">>.
+device_temp(DeviceName) ->
+    #{devname => DeviceName,
+      data => [
+               #{tstamp => <<"2014-04-07T13:58:11.104Z">>,
+                 value => 10},
+               #{tstamp => <<"2014-04-07T13:58:16.104Z">>,
+                 value => 20},
+               #{tstamp => <<"2014-04-07T13:58:21.104Z">>,
+                 value => 15}
+              ]}.
+
+device_humidity(DeviceName) ->
+    #{devname => DeviceName,
+      data => [
+               #{tstamp => <<"2014-04-07T13:58:11.104Z">>,
+                 value => 61},
+               #{tstamp => <<"2014-04-07T13:58:16.104Z">>,
+                 value => 59},
+               #{tstamp => <<"2014-04-07T13:58:21.104Z">>,
+                 value => 57}
+              ]}.
 
 %% Parsing ISO 8601 dates in JavaScript:
 %%   var d = new Date("2014-04-07T13:58:10.104Z");
