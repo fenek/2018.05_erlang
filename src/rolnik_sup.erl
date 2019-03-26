00a1dc2b86de6171e5a1d4f68dc1f4c2b88c4c7b
zofpolkowska <zofpolkowska@gmail.com>
2019-03-26 14:31:01 +0100

    Add devices under supervisor

--- src/rolnik_sup.erl
+++ src/rolnik_sup.erl
@@ -4,6 +4,8 @@
 
 -behavior(supervisor).
 
+-import(rolnik_device, [device_id_onewire/0]).
+
 % API
 -export([start_link/0]).
 
@@ -16,4 +18,14 @@ start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 %--- Callbacks -----------------------------------------------------------------
 
-init([]) -> {ok, { {one_for_all, 0, 1}, []} }.
+init([]) ->
+    timer:sleep(10000),
+    IdList = device_id_onewire(),
+    Children = [ worker(Id) || Id <- IdList ],
+    {ok, { {one_for_all, 0, 1}, Children }}.
+
+worker(Id) ->
+    #{id => Id,
+      start => {rolnik_agent, start_link, [Id]},
+      module => rolnik_agent,
+      restart => permanent}.
